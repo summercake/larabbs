24a3179ee307473cd47c32f29fa71de925663464
summercake@qq.com <summercake@qq.com>
2018-06-06 02:20:16 -0500

    Category topic list

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -1,7 +1,6 @@
 <nav class="navbar navbar-default navbar-static-top">
     <div class="container">
         <div class="navbar-header">
-
             <!-- Collapsed Hamburger -->
             <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
                 <span class="sr-only">Toggle Navigation</span>
@@ -9,19 +8,20 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-
             <!-- Branding Image -->
             <a class="navbar-brand" href="{{ url('/') }}">
                 LaraBBS
             </a>
         </div>
-
         <div class="collapse navbar-collapse" id="app-navbar-collapse">
             <!-- Left Side Of Navbar -->
             <ul class="nav navbar-nav">
-
+                <li class="{{ active_class(if_route('topics.index')) }}"><a href="{{ route('topics.index') }}">话题</a></li>
+                <li class="{{ active_class((if_route('categories.show') && if_route_param('category', 1))) }}"><a href="{{ route('categories.show', 1) }}">分享</a></li>
+                <li class="{{ active_class((if_route('categories.show') && if_route_param('category', 2))) }}"><a href="{{ route('categories.show', 2) }}">教程</a></li>
+                <li class="{{ active_class((if_route('categories.show') && if_route_param('category', 3))) }}"><a href="{{ route('categories.show', 3) }}">问答</a></li>
+                <li class="{{ active_class((if_route('categories.show') && if_route_param('category', 4))) }}"><a href="{{ route('categories.show', 4) }}">公告</a></li>
             </ul>
-
             <!-- Right Side Of Navbar -->
             <ul class="nav navbar-nav navbar-right">
                 <!-- Authentication Links -->
@@ -36,21 +36,18 @@
                             </span>
                             {{ Auth::user()->name }} <span class="caret"></span>
                         </a>
-
                         <ul class="dropdown-menu" role="menu">
                             <li>
                                 <a href="{{ route('users.edit', Auth::id()) }}">
                                     编辑资料
                                 </a>
                             </li>
-
                             <li>
                                 <a href="{{ route('logout') }}"
                                    onclick="event.preventDefault();
                                              document.getElementById('logout-form').submit();">
                                     退出登录
                                 </a>
-
                                 <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                                     {{ csrf_field() }}
                                 </form>
--- resources/views/topics/index.blade.php
+++ resources/views/topics/index.blade.php
@@ -1,20 +1,22 @@
 @extends('layouts.app')
-
-@section('title', '话题列表')
+@section('title', isset($category) ? $category->name : '话题列表')
 
 @section('content')
 
     <div class="row">
         <div class="col-lg-9 col-md-9 topic-list">
+            @if (isset($category))
+                <div class="alert alert-info" role="alert">
+                    {{ $category->name }} ：{{ $category->description }}
+                </div>
+            @endif
             <div class="panel panel-default">
-
                 <div class="panel-heading">
                     <ul class="nav nav-pills">
                         <li role="presentation" class="active"><a href="#">最后回复</a></li>
                         <li role="presentation"><a href="#">最新发布</a></li>
                     </ul>
                 </div>
-
                 <div class="panel-body">
                     {{-- 话题列表 --}}
                     @include('topics._topic_list', ['topics' => $topics])
@@ -23,7 +25,6 @@
                 </div>
             </div>
         </div>
-
         <div class="col-lg-3 col-md-3 sidebar">
             @include('topics._sidebar')
         </div>
