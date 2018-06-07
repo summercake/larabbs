f213438eb0e9a804698698bcaa5fcf48b8cd73b9
summercake@qq.com <summercake@qq.com>
2018-06-06 20:02:19 -0500

    User can create reply

--- resources/views/topics/_reply_box.blade.php
+++ resources/views/topics/_reply_box.blade.php
@@ -1,32 +1,12 @@
-<div class="reply-list">
-    @foreach ($replies as $index => $reply)
-        <div class=" media"  name="reply{{ $reply->id }}" id="reply{{ $reply->id }}">
-            <div class="avatar pull-left">
-                <a href="{{ route('users.show', [$reply->user_id]) }}">
-                    <img class="media-object img-thumbnail" alt="{{ $reply->user->name }}" src="{{ $reply->user->avatar }}"  style="width:48px;height:48px;"/>
-                </a>
-            </div>
-
-            <div class="infos">
-                <div class="media-heading">
-                    <a href="{{ route('users.show', [$reply->user_id]) }}" title="{{ $reply->user->name }}">
-                        {{ $reply->user->name }}
-                    </a>
-                    <span> •  </span>
-                    <span class="meta" title="{{ $reply->created_at }}">{{ $reply->created_at->diffForHumans() }}</span>
-
-                    {{-- 回复删除按钮 --}}
-                    <span class="meta pull-right">
-                        <a title="删除回复">
-                        <span class="glyphicon glyphicon-trash" aria-hidden="true"></span>
-                        </a>
-                    </span>
-                </div>
-                <div class="reply-content">
-                    {!! $reply->content !!}
-                </div>
-            </div>
+@include('common.error')
+<div class="reply-box">
+    <form action="{{ route('replies.store') }}" method="POST" accept-charset="UTF-8">
+        <input type="hidden" name="_token" value="{{ csrf_token() }}">
+        <input type="hidden" name="topic_id" value="{{ $topic->id }}">
+        <div class="form-group">
+            <textarea class="form-control" rows="3" placeholder="分享你的想法" name="content"></textarea>
         </div>
-        <hr>
-    @endforeach
+        <button type="submit" class="btn btn-primary btn-sm"><i class="fa fa-share"></i>回复</button>
+    </form>
 </div>
+<hr>
