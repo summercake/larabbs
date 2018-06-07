caf9ec54976c51d56297c29c5c9971c1572b3f8a
summercake@qq.com <summercake@qq.com>
2018-06-07 04:54:15 -0500

    Keep track of user last visited time

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,7 +4,6 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
-
 class Kernel extends ConsoleKernel
 {
     /**
@@ -19,14 +18,17 @@ class Kernel extends ConsoleKernel
     /**
      * Define the application's command schedule.
      *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+     * @param  \Illuminate\Console\Scheduling\Schedule $schedule
      * @return void
      */
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')
         //          ->hourly();
-        $schedule->command('larabbs:calculate-active-user')->hourly();
+        // 每隔一个小时执行一遍
+        $schedule -> command('larabbs:calculate-active-user') -> hourly();
+        // 每日零时执行一次
+        $schedule -> command('larabbs:sync-user-actived-at') -> dailyAt('00:00');
     }
 
     /**
@@ -36,8 +38,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands()
     {
-        $this->load(__DIR__.'/Commands');
-
+        $this -> load(__DIR__.'/Commands');
         require base_path('routes/console.php');
     }
 }
