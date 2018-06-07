0ee114dabc5eeed73708977bccba980e58031ece
summercake@qq.com <summercake@qq.com>
2018-06-06 20:30:30 -0500

    Notify author when topic replied

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,10 +4,22 @@ namespace App\Models;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-
+use Auth;
 class User extends Authenticatable
 {
-    use Notifiable;
+    use Notifiable {
+        notify as protected laravelNotify;
+    }
+
+    public function notify($instance)
+    {
+        // 如果要通知的人是当前用户，就不必通知了！
+        if ($this -> id == Auth ::id()) {
+            return;
+        }
+        $this -> increment('notification_count');
+        $this -> laravelNotify($instance);
+    }
 
     /**
      * The attributes that are mass assignable.
@@ -15,30 +27,41 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password', 'introduction','avatar'
+        'name',
+        'email',
+        'password',
+        'introduction',
+        'avatar',
     ];
-
     /**
      * The attributes that should be hidden for arrays.
      *
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token',
+        'password',
+        'remember_token',
     ];
 
     public function topics()
     {
-        return $this->hasMany(Topic::class);
+        return $this -> hasMany(Topic::class);
     }
 
     public function isAuthorOf($model)
     {
-        return $this->id == $model->user_id;
+        return $this -> id == $model -> user_id;
     }
 
     public function replies()
     {
-        return $this->hasMany(Reply::class);
+        return $this -> hasMany(Reply::class);
+    }
+
+    public function markAsRead()
+    {
+        $this->notification_count = 0;
+        $this->save();
+        $this->unreadNotifications->markAsRead();
     }
 }
